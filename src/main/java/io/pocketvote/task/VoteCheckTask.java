c34d1289f9d207a6b44c10c0424e354e972688ed
imjack <imjack@users.noreply.github.com>
2020-05-18 23:20:44 +0100

    check if username is a valid

--- src/main/java/io/pocketvote/task/VoteCheckTask.java
+++ src/main/java/io/pocketvote/task/VoteCheckTask.java
@@ -1,6 +1,7 @@
 package io.pocketvote.task;
 
 import java.util.LinkedHashMap;
+import java.util.regex.Pattern;
 
 import cn.nukkit.Server;
 import io.pocketvote.PocketVote;
@@ -10,7 +11,8 @@ import io.pocketvote.event.VoteEvent;
 public class VoteCheckTask extends ApiRequest {
 
     private PocketVote plugin;
-
+    private static final Pattern namePattern = Pattern.compile("^[a-zA-Z0-9_]{1,16}$");
+    
     public VoteCheckTask(PocketVote plugin) {
         super(plugin.isDev() ? "http://127.0.0.1:9000/v2/check" : "https://api.pocketvote.io/v2/check", "GET", "VOTE", null);
         this.plugin = plugin;
@@ -42,7 +44,9 @@ public class VoteCheckTask extends ApiRequest {
         }
 
         for(LinkedHashMap<String, String> vote : result.getVotes()) {
-            server.getPluginManager().callEvent(new VoteEvent(vote.get("player"), vote.get("ip"), vote.get("site")));
+            if(namePattern.matcher(vote.get("player")).find()) {
+                server.getPluginManager().callEvent(new VoteEvent(vote.get("player"), vote.get("ip"), vote.get("site")));
+            }
         }
 
         if(result.hasMeta()) plugin.startScheduler(result.getMeta().containsKey("frequency") ? (int) result.getMeta().get("frequency") : 60);
